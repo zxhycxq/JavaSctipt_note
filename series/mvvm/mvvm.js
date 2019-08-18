5c36d468d97e5de773961ecc410c5c4771b34afc
zxhycxq <lcyxlqn@163.com>
2019-08-18 20:49:04 +0800

    編譯模板

--- series/mvvm/mvvm.js
+++ series/mvvm/mvvm.js
@@ -7,17 +7,49 @@ function MvvmDemo(options = {}) {
   //this代理了ｔｈｉｓ．＿ｄａｔａ
   for (var prop in data) {
     
-    Object.defineProperty(this, prop,{
-      enumerable:true,
-      get(){
+    Object.defineProperty (this, prop, {
+      enumerable: true,
+      get() {
         return this._data[prop]
       },
-      set(newVal){
-        this._data[prop]=newVal
+      set(newVal) {
+        this._data[prop] = newVal
+      }
+    })
+  }
+  new Complie (options.el, this)
+}
+
+function Complie(el, vm) {
+  vm.$el=document.querySelector (el);
+  var fragment = document.createDocumentFragment ()
+  while (child = vm.$el.firstChild) {
+    fragment.appendChild (child)
+  }
+  replace (fragment)
+  
+  function replace(fragment) {
+    Array.from (fragment.childNodes).forEach (function (node) {
+      var test = node.textContent;
+      let reg = /\{\{(.*)\}\}/;
+      if (node.nodeType === 3 && reg.test (test)) {
+        console.log (`%c--reg-- `, 'color:blue;', RegExp.$1)
+        let arrs = RegExp.$1.split ('.') //[a,a]
+        let val = vm;
+        arrs.forEach (function (k) {
+          val = val[k]
+        });
+        
+        node.textContent = test.replace (/\{\{(.*)\}\}/, val)
+      }
+      if (node.childNodes) {
+        replace (node)
       }
     })
   }
   
+  
+  vm.$el.appendChild (fragment)
 }
 
 function Observe(data) {
@@ -30,9 +62,9 @@ function Observe(data) {
       },
       set(newVal) {
         if (newVal === val) {
-          return ;
+          return;
         }
-        val =newVal
+        val = newVal
       }
     })
   }
@@ -41,6 +73,7 @@ function Observe(data) {
 
 
 function observe(data) {
+  if (typeof data != 'object') return
   return new Observe (data)
 }
 
