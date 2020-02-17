850ed39571ef196cf8f0fc85e8dd0c000e76a390
zxhycxq <lcyxlqn@163.com>
2020-02-17 10:59:09 +0800

    描述: 日期: 作者:

--- promise/1.js
+++ promise/1.js
@@ -1,4 +1,17 @@
-/**
- * Created by cxq on 2019/11/03.
- * 十一月
- */
+function isType(type) {
+  return function (params) {
+    console.log(`%c--params-- `, 'color:blue;', params,Object.prototype.toString.call(params))
+    return Object.prototype.toString.call(params)==`[object ${type}]`
+  }
+}
+
+let  isString=isType('String')
+
+console.log(`%c--isString({})-- `, 'color:blue;', isString('red'))
+
+//回调的一个问题
+// 1、无法捕获错误 try catch
+//2. 不能return
+//3、 回调地狱  事件发布订阅解决
+
+
